142edcad6c358026c8a6c66880bade13147a6f63
AnukulPr1me <anukul_chandra@outlook.com>
2024-06-02 08:58:04 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,14 @@ function Home() {
           HeroCarousel
         </div>
       </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {['Apple Iphone 15']}
+        </div>
+
+      </section>
     </>
   );
 }
