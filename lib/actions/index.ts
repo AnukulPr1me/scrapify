b087c58f61e9a420b1b6f80f1e23b5d7994bac39
AnukulPr1me <anukul_chandra@outlook.com>
2024-06-05 16:52:39 +0530

    added scraper

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -4,7 +4,7 @@ export async function scrapeAndStoreProduct(porductUrl:string) {
     if(!porductUrl) return;
 
     try{
-
+      const scrapedProduct = await scrapeAmazonProduct(productUrl)
     } catch (error) {
         throw new Error('failed to update product:$(error.message)') 
     }
