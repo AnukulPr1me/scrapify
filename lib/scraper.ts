2f66d5f5ef19d9de30a7309e1259f5bef6ef768f
AnukulPr1me <anukul_chandra@outlook.com>
2024-06-22 23:36:11 +0530

    added to view original price

--- lib/scraper.ts
+++ lib/scraper.ts
@@ -34,7 +34,15 @@ export async function scrapeAmazonProduct(url: string) {
       $('.a-price.a-text-price')
     );
 
-    console.log({title, currentPrice});
+    const originalPrice = extractPrice(
+      $('#priceblock_ourprice'),
+      $('.a-price.a-text-price span.a-offscreen'),
+      $('#listPrice'),
+      $('#priceblock_dealprice'),
+      $('.a-size-base.a-color-price')
+    )
+
+    console.log({title, currentPrice, originalPrice});
   } catch (error: any) {
     throw new error("Failed to scrape the product: ${error.message}");
   }
